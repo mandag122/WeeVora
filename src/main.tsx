53e66439b5df44cace856c7ad3b666f21cf0994f
mandag122 <amanda.ugaste@gmail.com>
2026-02-01 11:50:16 -0600

    Add PostHog integration to main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
+import "@/lib/posthog";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
