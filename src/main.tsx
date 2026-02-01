b0a473d363c0fba4256a51d8437846fc03fdc6c5
mandag122 <amanda.ugaste@gmail.com>
2026-02-01 00:48:47 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+import { SessionProvider } from "@/context/SessionContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <App />
+      </SessionProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
